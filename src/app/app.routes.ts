6d96a0d7ba132364a8ac9f22be43db85ca6868ea
Ahmed-BenHajAmor <142888862+Ahmed-BenHajAmor@users.noreply.github.com>
2024-08-20 17:59:13 +0100

    Update app.routes.ts
    import fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-
-import { TasksComponent } from "/workspaces/codespaces-blank/todoApp/src/app/tasks/tasks.component";
-import { DoneTasksComponent } from "/workspaces/codespaces-blank/todoApp/src/app/done-tasks/done-tasks.component";
+import { TasksComponent } from './tasks/tasks.component';
+import { DoneTasksComponent } from './done-tasks/done-tasks.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/tasks', pathMatch: 'full' },
     { path: 'tasks', component: TasksComponent }, 
     { path: 'done', component: DoneTasksComponent } 
 ];
