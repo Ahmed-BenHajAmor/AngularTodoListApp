1f3cfcac28ea37ea832a43938588456973be3155
Ahmed-BenHajAmor <142888862+Ahmed-BenHajAmor@users.noreply.github.com>
2024-08-21 13:55:08 +0100

    Update tasks.component.ts

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -4,6 +4,7 @@ import { NgFor, NgIf, NgStyle } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LinksComponent } from '../links/links.component';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-tasks',
@@ -23,6 +24,8 @@ export class TasksComponent {
 
   ngOnInit(): void {
     this.todosService.getTodos().subscribe(todos => this.todosList = todos);
+    
+    
   }
 
   getTimeFromTodo(date : Date) : string{
@@ -35,11 +38,41 @@ export class TasksComponent {
   }
 
   removeTodo(todoID : string) : void{
-    this.todosService.removeTodo(todoID)
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.todosService.removeTodo(todoID)
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        );
+      }
+    });
   }
 
   checkTaskInPast(todo : Todo){
     return new Date(`${todo.date} ${todo.time}`) < new Date()
+  }
+
+  getDateAndTime(date: string){
+    const newDate = new Date(date)
+    const year = newDate.getFullYear();
+    const month = newDate.getMonth() + 1; // Add 1 since months are zero-indexed
+    const day = newDate.getDate();
+
+    const hours = newDate.getHours();
+    const minutes = newDate.getMinutes();
+    return `${year}-${month}-${day}, ${hours}:${minutes}`
+
+    
   }
 }
 
