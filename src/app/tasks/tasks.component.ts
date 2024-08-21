3b9f8421e6f92ac4f651e3d2065e4d51b14e22cf
Ahmed-BenHajAmor <142888862+Ahmed-BenHajAmor@users.noreply.github.com>
2024-08-21 13:08:18 +0000

    final-commit

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -63,14 +63,16 @@ export class TasksComponent {
   }
 
   getDateAndTime(date: string){
-    const newDate = new Date(date)
+    const newDate = new Date(date);
     const year = newDate.getFullYear();
-    const month = newDate.getMonth() + 1; // Add 1 since months are zero-indexed
-    const day = newDate.getDate();
-
-    const hours = newDate.getHours();
-    const minutes = newDate.getMinutes();
-    return `${year}-${month}-${day}, ${hours}:${minutes}`
+    const month = String(newDate.getMonth() + 1).padStart(2, '0'); 
+    const day = String(newDate.getDate()).padStart(2, '0');           
+    const hours = String(newDate.getHours()).padStart(2, '0');     
+    const minutes = String(newDate.getMinutes()).padStart(2, '0'); 
+    
+    
+    return `${year}-${month}-${day}, ${hours}:${minutes}`;
+    
 
     
   }
